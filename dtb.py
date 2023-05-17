f9ed6907876e3aa98eb52d3b4492b277201a35b1
talavodoi1234567 <vodoi7232@gmail.com>
2023-05-17 22:04:36 +0700

    + crypto

--- dtb.py
+++ dtb.py
@@ -2,6 +2,15 @@ from peewee import *
 
 db = MySQLDatabase('sql12617601', host='sql12.freesqldatabase.com', port=3306, user='sql12617601', password='WgNvzqpavt')
 
+class Customer(Model):
+    ID = IntegerField()
+    Name = BlobField()
+    DoB = DateField()
+    monthly_ticket = DateField()
+    class Meta:
+        database = db
+
+
 if __name__ == '__main__':
     try:
         db.connect()
