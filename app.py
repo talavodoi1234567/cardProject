369d19eb7ead189850b767d9de06cfbd272b91f5
talavodoi1234567 <vodoi7232@gmail.com>
2023-05-25 20:17:37 +0700

    + api kết nối với database

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-import traceback
+from datetime import date
 
-from flask import Flask, json, jsonify
-from dtb import db
+from flask import Flask, jsonify, request
+from dtb import *
 
 app = Flask(__name__)
+
+
+def default(obj):
+    if isinstance(obj, date):
+        return obj.strftime('%d-%m-%Y')
+    raise TypeError(f'Object of type {type(obj)} is not JSON serializable')
 
 
 @app.route('/')
@@ -26,8 +32,80 @@ def testdtb():
         })
     except Exception:
         return jsonify({
-            'connect': traceback.print_exc()
+            'connect': False
         })
+
+@app.route('/check_card', methods=['GET'])
+#check thẻ đã được cắm vào hay chưa
+#deploy chưa có thẻ nên làm tạm
+def check_card():
+    return jsonify({
+        'connect': True,
+        'data': jsonify(select_user(1))
+    })
+
+
+@app.route('/create_card', methods=['POST'])
+# tạo thẻ
+def create_card():
+    try:
+        data = request.get_json()
+        name = data.get('Name')
+        cmnd = data.get('CMND')
+        gender = data.get('Gender')
+        picture = data.get('Picture')
+        dob = data.get('DoB')
+        exp_date_ticket = data.get('Exp_date_ticket')
+        balance = data.get('Balance')
+        exp_date_card = data.get('Exp_date_card')
+        types_of_ticket = data.get('Types_of_ticket')
+        add_user(name, cmnd, gender, picture, dob, exp_date_ticket, balance, exp_date_card, types_of_ticket)
+        return jsonify({
+            'message': True
+        })
+    except Exception:
+        return jsonify({
+            'message': False
+        })
+
+
+@app.route('/edit_card', methods=['POST'])
+def edit_user():
+    try:
+        id = request.args.get('ID')
+        data = request.get_json()
+        name = data.get('Name')
+        cmnd = data.get('CMND')
+        gender = data.get('Gender')
+        picture = data.get('Picture')
+        dob = data.get('DoB')
+        exp_date_ticket = data.get('Exp_date_ticket')
+        balance = data.get('Balance')
+        exp_date_card = data.get('Exp_date_card')
+        types_of_ticket = data.get('Types_of_ticket')
+        edit_user(id, name, cmnd, gender, picture, dob, exp_date_ticket, balance, exp_date_card, types_of_ticket)
+        return jsonify({
+            'message': True
+        })
+    except Exception:
+        return jsonify({
+            'message': False
+        })
+
+
+@app.route('/delete_card', methods=['POST'])
+def delete_card():
+    try:
+        id = request.args.get('ID')
+        delete_user(id)
+        return jsonify({
+            'message': True
+        })
+    except Exception:
+        return jsonify({
+            'message': True
+        })
+
 
 if __name__ == '__main__':
     app.run()
--- dtb.py
+++ dtb.py
@@ -1,4 +1,5 @@
 from peewee import *
+# from AES_cryption import data_decrypt, data_encrypt
 
 db = PostgresqlDatabase(
     'dtb_name',
@@ -8,15 +9,69 @@ db = PostgresqlDatabase(
     port=5432
 )
 
-# class Customer(Model):
-#     ID = IntegerField()
-#     Name = BlobField()
-#     DoB = DateField()
-#     monthly_ticket = DateField()
-#     class Meta:
-#         database = db
+
+class NoEncryptedCustomer(Model):
+    ID = AutoField()
+    Name = TextField()
+    CMND = TextField()
+    Gender = BooleanField()
+    Picture = TextField()
+    DoB = TextField()
+    Exp_date_ticket = TextField()
+    Balance = IntegerField()
+    Exp_date_card = TextField()
+    Types_of_ticket = IntegerField()
+
+    class Meta:
+        table_name = 'No_Encrypted_Customer'
+        database = db
+
+
+def select_user(id):
+    data = NoEncryptedCustomer.select().where(NoEncryptedCustomer.ID == id)
+    cus = [item.__dict__['__data__'] for item in data]
+    customer = cus[0]
+    return {
+        'ID': customer['ID'],
+        'Name': customer['Name'],
+        'CMND': customer['CMND'],
+        'Gender': customer['Gender'],
+        'Picture': customer['Picture'],
+        'DoB': customer['DoB'],
+        'Exp_date_ticket': customer['Exp_date_ticket'],
+        'Balance': customer['Balance'],
+        'Exp_date_card': customer['Exp_date_card'],
+        'Types_of_ticket': customer['Types_of_ticket']
+    }
+
+
+def add_user(name, cmnd, gender, picture, dob, exp_date_ticket, balance, exp_date_card, types_of_ticket):
+    record = NoEncryptedCustomer(Name=name, CMND=cmnd, Gender=gender, Picture=picture, DoB=dob, Exp_date_ticket=exp_date_ticket, Balance=balance, Exp_date_card=exp_date_card, Types_of_ticket=types_of_ticket)
+    record.save()
+
+
+def edit_user(id, name, cmnd, gender, picture, dob, exp_date_ticket, balance, exp_date_card, types_of_ticket):
+    record = NoEncryptedCustomer.get_by_id(id)
+    record.Name = name
+    record.CMND = cmnd
+    record.Gender = gender
+    record.Picture = picture
+    record.DoB = dob
+    record.Exp_date_ticket = exp_date_ticket
+    record.Balance = balance
+    record.Exp_date_card = exp_date_card
+    record.Types_of_ticket = types_of_ticket
+    record.save()
+
+
+def delete_user(id):
+    record = NoEncryptedCustomer.get_by_id(id)
+    record.delete_instance()
 
 
 if __name__ == '__main__':
-    db.connect()
-    print('Connected to database')
+    try:
+        db.connect()
+        print('Connected to database')
+    except Exception:
+        print('Failed to connect to the database')
